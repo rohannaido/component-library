d2a0d038029c091270298da5b613da1c5b0c789b
rohannaido <87189343+rohannaido@users.noreply.github.com>
2023-11-01 00:45:33 +0530

    Filtering in dropdown and form value accessing

--- src/app/multi-column-drop-down-page/multi-column-drop-down-page.component.ts
+++ src/app/multi-column-drop-down-page/multi-column-drop-down-page.component.ts
@@ -1,37 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-multi-column-drop-down-page',
   templateUrl: './multi-column-drop-down-page.component.html',
   styleUrls: ['./multi-column-drop-down-page.component.css']
 })
-export class MultiColumnDropDownPageComponent {
+export class MultiColumnDropDownPageComponent implements OnInit {
 
   dropDownColumnConfig: any[] = [
     {
-      title: "Brand",
-      bindKey: "company",
+      title: "Doctor Name",
+      bindKey: "doctorName",
+      displayItem: true,
     },
     {
-      title: "Model",
-      bindKey: "model",
-      displayItem: true,
+      title: "Dept.",
+      bindKey: "department",
+    },
+    {
+      title: "Exp. (yrs)",
+      bindKey: "experience",
     }
   ];
 
   dropDownDataList : any [] = [
     {
-      company: "Maruti",
-      model: "Alto",
+      docId: 1,
+      doctorName: "John",
+      department: "Cardiology",
+      experience: "10",
     },
     {
-      company: "Tata",
-      model: "Nexon",
+      docId: 2,
+      doctorName: "Ken",
+      department: "Cardiology",
+      experience: "12",
     },
     {
-      company: "Ford",
-      model: "Endeavour",
+      docId: 3,
+      doctorName: "Rob",
+      department: "ENT",
+      experience: "5",
+    },
+    {
+      docId: 4,
+      doctorName: "Tom",
+      department: "Orthopedic",
+      experience: "16",
+    },
+    {
+      docId: 5,
+      doctorName: "Kunal",
+      department: "ENT",
+      experience: "3",
+    },
+    {
+      docId: 6,
+      doctorName: "Michael",
+      department: "Cardiology",
+      experience: "19",
     },
   ]
+
+  doctor: any | undefined;
+  displayValue: string = "";
+
+  ngOnInit(): void {
+  }
+
+  handleDropDownChange(value: any){
+    this.displayValue = JSON.stringify(value);
+  }
 
 }
--- src/app/multi-column-drop-down/multi-column-drop-down.component.ts
+++ src/app/multi-column-drop-down/multi-column-drop-down.component.ts
@@ -1,38 +1,96 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, OnInit, forwardRef, EventEmitter } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'multi-column-drop-down',
   templateUrl: './multi-column-drop-down.component.html',
-  styleUrls: ['./multi-column-drop-down.component.css']
+  styleUrls: ['./multi-column-drop-down.component.css'],
+  providers: [
+    {
+       provide: NG_VALUE_ACCESSOR,
+       useExisting: forwardRef(() => MultiColumnDropDownComponent),
+       multi: true
+    }
+ ]
 })
-export class MultiColumnDropDownComponent {
+export class MultiColumnDropDownComponent implements OnInit, ControlValueAccessor {
   
   @Input() columnConfig: any[] = [];
   @Input() dataList: any[] = [];
 
+  @Output() valueChanged: EventEmitter<any> = new EventEmitter<any>();
+
+  disabled: boolean = false;
+
+  filteredDropDownList: any[] = [];
+
+  selectedItem: any;
   selectedItemDisplay: any;
   displayKey: string | undefined;
   showDataPanel: boolean = false;
 
   constructor(){
+  }
+
+  ngOnInit(){
     this.columnConfig.forEach((columnItem: any) => {
       if(columnItem.displayItem === true){
         this.displayKey = columnItem.bindKey;
         return;
       }
     })
-  }
 
+    //Search string
+    this.dataList.forEach((dataItem) => {
+      dataItem.searchString = Object.values(dataItem).join('');
+    })
+
+    
+    this.filteredDropDownList = this.dataList;
+
+  }
   
   handleInputClick(){
     this.showDataPanel = true;
   }
 
+  handleInputType(){
+    this.filterDropDownData(this.selectedItemDisplay);
+  }
+
+  filterDropDownData(searchInput: string){
+    this.filteredDropDownList = searchInput ? this.dataList.filter((dataItem) => dataItem.searchString.toLowerCase().includes(searchInput)) : this.dataList;
+  }
+
   handleDataItemClick(dataItem: any) {
     this.showDataPanel = false;
+    this.selectedItem = dataItem;
+    
+    this.valueChanged.emit(this.selectedItem);
+    this.onChange(this.selectedItem);
+
     if(this.displayKey){
       this.selectedItemDisplay = dataItem[this.displayKey];
     }
+  }
+
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  writeValue(value: string): void {
+    this.selectedItem = value;
   }
 
 }
