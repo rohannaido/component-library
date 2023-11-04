8ef5c3d53b82450ec7bb7ce07fa24dbcb90d9701
rohannaido <87189343+rohannaido@users.noreply.github.com>
2023-11-05 00:34:33 +0530

    side nav active display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private router: Router) { }
+
+  ngOnInit(){
+    setTimeout(() => {
+      const currentRoute = this.router.url;
+      this.toggleNavListActiveRoute(currentRoute);
+    })
+  }
+
   sideMenuOpened: boolean = true;
+
+  navLinkList: any[] = [
+    {
+      route: "/multi-column-drop-down-page",
+      title: "Multi column drop down",
+    },
+    {
+      route: "/multi-column-multi-select-page",
+      title: "Multi column multi select",
+    },
+    {
+      route: "/data-table-page",
+      title: "Data Table",
+    },
+  ]
+
+  handleNavListItemClick(navLinkItem: any){
+    this.toggleNavListActiveRoute(navLinkItem.route);
+  }
+
+  toggleNavListActiveRoute(route: string){
+    this.navLinkList = this.navLinkList.map((item) => {
+      if(route === item.route){
+        item.isActive = true;
+      }
+      else {
+        item.isActive = false;
+      }
+      return item;
+    })
+  }
+
+
 }
